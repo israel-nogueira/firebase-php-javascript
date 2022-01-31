039f9ccba91dc9c5b7a7c0b6048321d3d09be850
GOSTOSÃO <israel@feats.com.br>
2022-01-31 20:07:41 -0300

    update

--- firebase.js
+++ firebase.js
@@ -9,84 +9,126 @@ const firebaseConfig = {
     measurementId: "G-GHERTHERTV"
   };
 
-var websocket = {
-    firebase:{},
-    database:{},
-    initialize:function(e){
-        websocket.firebase = firebase;
-        if(websocket.firebase.apps.length==0){
-            websocket.firebase.initializeApp(firebaseConfig);
-            websocket.database = websocket.firebase.database().ref('/');
-        }
-    },
-    base:function(e){
-        websocket.initialize()
-        websocket.database = websocket.firebase.database().ref(e);
-        return websocket;
-    },
-
-    set:async function(e){
-        websocket.initialize()
-        return await new Promise((resolve, reject) => {
-            websocket.database.set(e).then(function(snapshot) {
-                resolve(snapshot);
-            })
-        }) 
-    },
-    delete:async function(){
-        websocket.initialize()
-        return await new Promise((resolve, reject) => {
-            websocket.database.set(null).then(function(snapshot) {
-                resolve(snapshot);
-            })
-        }) 
-    },
-    push:async function(key,value){
-        websocket.initialize()
-        return await new Promise((resolve, reject) => {
-            websocket.database.once("value").then(function(snapshot) {
-                    var result = snapshot.val()
-                    if(result==null){
-                        websocket.database.set(JSON.parse('{"'+key+'":'+JSON.stringify(value)+'}'))
-                    }else{
-                        var result2 = JSON.parse('{"'+key+'":'+JSON.stringify(value)+'}')
-                        var merged = {...result, ...result2};
-                        websocket.database.set(merged)
-                    }
-                resolve(merged);
-            })
-        }) 
-    },
-    get:async function(){
-        websocket.initialize()
-        return await new Promise((resolve, reject) => {
-            websocket.database.once("value").then(function(snapshot) {
-                resolve(snapshot.val());
-            })
-        }) 
-    },
-    onUpdate:function(retorno){
-        websocket.initialize()
-        websocket.database.on('value', function(snapshot) {
-            retorno(snapshot.val());
-        });
-    },
-    onChanged:function(retorno){
-        websocket.initialize()
-        websocket.database.on('child_changed', function(snapshot) {
-            retorno(snapshot.val());
-        });
-    },
-    onAdded:function(retorno){
-        websocket.initialize()
-        websocket.database.on('child_added', function(snapshot) {
-            retorno(snapshot.val());
-        });
-    },
-    onDelete:function(retorno){
-        websocket.initialize()
-        websocket.database.on('child_removed', function(snapshot) {
-            retorno(snapshot.val());
-        });
+window.newFirebase = async function (instancia = false) {
+    randomString = (n, r = '') => {
+        while (n--) r += String.fromCharCode((r = Math.random() * 62 | 0, r += r > 9 ? (r < 36 ? 55 : 61) : 48));
+        return r;
+    };
+    if (instancia == false) {
+        var instancia = 'firebase_' + randomString(50);
+    } else {
+        var instancia = 'firebase_' + instancia;
     }
+    if (typeof window[instancia] == 'undefined') {
+        window[instancia] = {};
+    }
+
+    window[instancia] = {
+        firebase: {},
+        database: {},
+        initialize: function (e) {
+            window[instancia].firebase = window.firebase;
+            if (window[instancia].firebase.apps.length == 0) {
+                window[instancia].firebase.initializeApp(firebaseConfig);
+                window[instancia].database = window[instancia].firebase.database().ref('/');
+            }
+        },
+        base: function (e) {
+            window[instancia].initialize();
+            window[instancia].database = window[instancia].firebase.database().ref(e);
+            return window[instancia];
+        },
+        set: async function (e) {
+            window[instancia].initialize();
+            return await new Promise((resolve, reject) => {
+                window[instancia].database.set(e).then(function (snapshot) {
+                    resolve(e);
+                })
+            })
+        },
+
+        delete: async function () {
+            window[instancia].initialize();
+            return await new Promise((resolve, reject) => {
+                window[instancia].database.set(null).then(function (snapshot) {
+                    resolve(snapshot);
+                })
+            })
+        },
+
+        push: async function () {
+            window[instancia].initialize();
+            var parametros = arguments
+            return await new Promise((resolve, reject) => {
+                window[instancia].database.once('value').then(function (snapshot) {
+                    var result = snapshot.val()
+                    if (result == null) { result = []; }
+                    if (parametros.length == 1) {
+                        result.push(parametros[0]);
+                    } else {
+                        if (typeof (result[parametros[0]]) != "undefined") {
+                            if (Object.prototype.toString.call(result[parametros[0]]) == '[object Array]') {
+                                result[parametros[0]].push(parametros[1]);
+                            } else if (Object.prototype.toString.call(result[parametros[0]]) == '[object Object]') {
+                                result[parametros[0]] = parametros[1];
+                            } else if (Object.prototype.toString.call(result[parametros[0]]) == '[object String]' || Object.prototype.toString.call(result[parametros[0]]) == '[object Number]') {
+                                var newArray = [result[parametros[0]]];
+                                result[parametros[0]] = newArray;
+                            };
+                        } else {
+                            result[parametros[0]] = parametros[1];
+                        }
+                    }
+                    window[instancia].database.set(result)
+                    resolve(result);
+                });
+            });
+        },
+        get: async function () {
+            window[instancia].initialize();
+            return await new Promise((resolve, reject) => {
+                window[instancia].database.once('value').then(function (snapshot) {
+                    resolve(snapshot.val());
+                })
+            })
+        },
+        onUpdate: function (retorno) {
+            window[instancia].initialize();
+            window[instancia].database.on('value', function (snapshot) {
+                retorno(snapshot.val());
+            });
+        },
+        onChanged: function (retorno) {
+            window[instancia].initialize();
+            window[instancia].database.on('child_changed', function (snapshot) {
+                retorno(snapshot.val());
+            });
+        },
+        onAdded: function (retorno) {
+            window[instancia].initialize();
+            window[instancia].database.on('child_added', function (snapshot) {
+                retorno(snapshot.val());
+            });
+        },
+        onDelete: function (retorno) {
+            window[instancia].initialize();
+            window[instancia].database.on('child_removed', function (snapshot) {
+                retorno(snapshot.val());
+            });
+        }
+    };
+
+    console.log("Firebase connected:")
+    console.log("Instance:", instancia)
+    return window[instancia];
 }
+
+
+
+var minhaInstancia = await window.newFirebase('XABLAU');
+
+minhaInstancia.base('usuario/profile/data').set({ 'avatar': 'hog87h50gh45hgy.png' }).then(function (snapshot) {
+    console.log("push:", snapshot)
+}, function (snapshot) {
+    console.log("push:", snapshot)
+})
