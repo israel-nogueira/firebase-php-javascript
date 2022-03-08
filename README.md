ce28afa92840ff06dbf2d88abed201cc300ce843
Israel Nogueira <israel@feats.com.br>
2022-03-08 20:43:25 -0300

    update

--- README.md
+++ README.md
@@ -1,29 +1,39 @@
 # Firebase Realtime (PHP+JAVASCRIPT)
 
-É uma classe intermediária para o repositório  **luqmanrom/firebase-php** que faz em PHP a autenticação e o CRUD básico.
+  
 
+É uma classe intermediária para o repositório **luqmanrom/firebase-php** que faz em PHP a autenticação e o CRUD básico.
 Modelei essa classe para que pudesse utilizar em meu projeto particular.
-
 Basicamente eu utilizo para criar eventos no front-end enquanto atualizo no Back.
-
 Porém pode ser utilizada de varias maneiras.
+Aproveitem!
 
-Aproveitem! 
+  
 
 ## Dependência
 
+  
+
 Faça download via Composer da classe original
+
 ```
+
 composer require geckob/firebase
+
 ```
+
+  
 
 ## Início
 
+  
+  
 
 ### 1. Gerar chave secreta da conta de serviço
 
-** Para gerar o arquivo secreto, siga estas etapas**
+  
 
+** Para gerar o arquivo secreto, siga estas etapas**
 
 1. Vá até [Firebase Console](https://console.firebase.google.com/)
 2. Crie seu projeto
@@ -34,55 +44,61 @@ composer require geckob/firebase
 7. Faça download do arquivo "secret.json"
 8. Proteja o acesso direto a seu arquivo com o apache ou NGIX
 
+  
 **1.2 Utilize o aqruivo na classe PHP**
+
 ```php
-    $firebase = new \Geckob\Firebase\Firebase('./secret.json');
+
+$firebase =  new \Geckob\Firebase\Firebase('./secret.json');
+
 ```
+
 ### 2. PHP
+
+  
 
 A operação CRUD no Firebase Database é baseada no [Firebase REST API Docs](https://www.firebase.com/docs/rest-api.html).
 
 Supondo que a autenticação seja feita com sucesso, siga os passos:
 
+  
 ```php
 
-	// Setamos o path da base
-	websocket::base('/')
-	
-	// Crie um novo nó, se o nó já existir, ele atualizará o valor 
-	->set('testObject','testValue')
-	
-	// Suporta vários nós, se não existir, ele criará o nó
-	->set('testObject/testKey','testValueObject')
-	
-	// Também poderá criar arrays com sub nós
-	->set('testObject',['testKey'=>'value','testValueObject'=>'other value'])
+ // Setamos o path da base
+websocket::base('/')
+// Crie um novo nó, se o nó já existir, ele atualizará o valor
+->set('testObject','testValue')
+// Suporta vários nós, se não existir, ele criará o nó
+->set('testObject/testKey','testValueObject')
+// Também poderá criar arrays com sub nós
+->set('testObject',['testKey'=>'value','testValueObject'=>'other value'])
 
-	/*
-	* Função push adaptada para trabalhar da maneira a qual eu acho correta
-	* Original insere uma chave randômica
-	*/
-	
-	//Se for uma string ('value'), ele transforma em array e adiciona o valor
-	->push('testKey','STRING QUALQUER') // return ['value','STRING QUALQUER']
+/*
+* Função push adaptada para trabalhar da maneira a qual eu acho correta
+* Original insere uma chave randômica
+*/
 
-	//Se for uma array ["teste"=>"123"], ele insere um novo item 
-	->push('testObject','STRING QUALQUER') // return ["teste"=>"123","0"=>"STRING QUALQUER"]
-	
-	//Se for uma array ["teste"=>"123"], e o novo dado for outra array, ele insere um novo item 
-	->push('testObject',['string'=>'test']) // return ["teste"=>"123","string"=>"test"]
+//Se for uma string ('value'), ele transforma em array e adiciona o valor
+->push('testKey','STRING QUALQUER') // return ['value','STRING QUALQUER']
+//Se for uma array ["teste"=>"123"], ele insere um novo item
+->push('testObject','STRING QUALQUER') // return ["teste"=>"123","0"=>"STRING QUALQUER"]
+//Se for uma array ["teste"=>"123"], e o novo dado for outra array, ele insere um novo item
+->push('testObject',['string'=>'test']) // return ["teste"=>"123","string"=>"test"]
+// exclui o nó inteiro
+->delete('Estatico/aaa/teste2')
 
-	// exclui o nó inteiro
-	->delete('Estatico/aaa/teste2')
-
-	// Retorna o nó pedido
-	->get('testObject'); // return ['string'=>'test']
+// Retorna o nó pedido
+->get('testObject'); // return ['string'=>'test']
 
 ```
 
+  
+
 ### 3. JAVASCRIPT
 
+  
 **Pegar as credenciais de acesso da API**
+
 Para gerar as credenciais, siga estas etapas
 
 1. Vá até [Firebase Console](https://console.firebase.google.com/)
@@ -92,89 +108,92 @@ Para gerar as credenciais, siga estas etapas
 5. Vá e clique na aba "Geral"
 6. Role até em baixo, e clique no radiobox "Firebase SDK snippet"
 7. Copie as credenciais e cole no topo do seu arquivo **firebase.js**
-
 Agora que nosso Javascript está 100%, vamos as funções no arquivo **index.html**
 
 ```javascript
+// DESSA FORMA PODEREMOS CRIAR VARIAS INSTANCIAS SEPARADAMENTE
+// E CONTROLAR INDIVIDUALMENTE CADA UMA DELAS
 
-
-    // Aqui recebemos o valor do nó
-	window.websocket.base('/pessoas/eu').get().then(function(retorno) {
-	 	console.log(retorno);
-	 });
-	 
-    /*
-	* Função push adaptada para trabalhar da maneira a qual eu acho correta
-	* Original insere uma chave randômica
-	* aqui a gente realmente insere um ítem 
-	*/
-	window.websocket.base('/').push('associado',[1,2,3,4]).then(function(snapshot) {
-	 	console.log(snapshot);
-	 });
-	 
-    // Aqui setamos um novo valor ao nó, e retornamos o conteudo
-    window.websocket.base('/pessoas/eu').set({nome: 'João',sobrenome: 'Silva'}).then(function(retorno) {
-    	window.websocket.base('/pessoas/eu').get().then(function(retorno) {
-    		console.log(retorno);
-    	});
-    });
-	// exclui o nó 
-	window.websocket.base('/pessoas/eu').delete().then(function() {
-	 	console.log('deletado');
-	 });
-
+var  NOVA_INSTANCIA  =  await  window.newFirebase.novaInstancia('NOVA_INSTANCIA');
 
 //################################################################################
-//  LISTNERS
+// AÇÕES REATIVAS
 //################################################################################
 
-	// UPDATE RETORNA  INCLUSOES, EXCLUSOES E ALTERAÇÕES	
-	window.websocket.onUpdate(function(snapshot) {
-		console.log('onUpdate',snapshot);
-	});
+/*
+* Função push adaptada para trabalhar da maneira a qual eu acho correta
+* Original insere uma chave randômica
+* aqui a gente realmente insere um ítem
+*/
+NOVA_INSTANCIA.base('root/teste1').push(['aaa','bbb','ccc','dddd'])
+.then(function (snapshot) {
+console.log("Sucesso push:", snapshot)
+}).catch(function (snapshot) {
+console.log("Falha push:", snapshot)
+})
 
-	// RETORNA APENAS ALTERAÇÕES DE VALORES
-	window.websocket.onChanged(function(snapshot) {
-		console.log('onChanged',snapshot);
-	});
+  
 
+// Aqui setamos um novo valor ao nó, e retornamos o conteudo
+NOVA_INSTANCIA.base('/pessoas/eu').set({nome: 'João',sobrenome: 'Silva'})
+.then(function(retorno) {
+console.log("Sucesso set:",retorno);
+}).catch(function(retorno) {
+console.log("Falha set:",retorno);
+})
 
-	// RETORNA ITENS ADICIONADOS
-	window.websocket.onAdded(function(snapshot) {
-		console.log('onAdded',snapshot);
-	});
+// Para excluir o nó baste:
+NOVA_INSTANCIA.base('/pessoas/eu').delete()
+.then(function() {
+console.log('deletado');
+}).catch(function(retorno) {
+console.log("Falha delete");
+})
 
+//################################################################################
+// LISTNERS
+//################################################################################
 
-	// RETORNA ITENS DELETADOS
-	window.websocket.onDelete(function(snapshot) {
-		console.log('onDelete',snapshot);
-	});
+// CRIAMOS UMA NOVA INSTANCIA
+var  minhaInstancia  =  await  window.newFirebase.novaInstancia('NOVA_INSTANCIA');
 
-	
+// UPDATE RETORNA INCLUSOES, EXCLUSOES E ALTERAÇÕES
+minhaInstancia.base('usuario').onUpdate(function(snapshot) {
+	console.log('onUpdate',snapshot);
+});
+
+// RETORNA APENAS ALTERAÇÕES DE VALORES
+minhaInstancia.base('usuario').onChanged(function(snapshot) {
+	console.log('onChanged',snapshot);
+});
+
+// RETORNA ITENS ADICIONADOS
+minhaInstancia.base('usuario').onAdded(function(snapshot) {
+	console.log('onAdded',snapshot);
+});
+
+// RETORNA ITENS DELETADOS
+minhaInstancia.base('usuario').onDelete(function(snapshot) {
+	console.log('onDelete',snapshot);
+});
 
 ```
-
 ## AGRADECIMENTO
-
 Quero agradecer o [Luqman Rom](https://github.com/luqmanrom) que disponibilizou publicamente essa classe em PHP que me foi tão útil
 
-
-## License 
+## License
 
 #### The MIT License (MIT)
 ```
 Copyright (c) 2012-2016 Tamas Kalman
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -182,7 +201,5 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
-```	
 
-
-
+```
--- firebase.js
+++ firebase.js
@@ -1,133 +1,223 @@
-const firebaseConfig = {
-    apiKey: "gfjhsfghertbsrfnfdh5y6u45686wh635tbsrh5hgs5eg",
-    authDomain: "{MY-APP}.firebaseapp.com",
-    databaseURL: "https://{MY-APP}.firebaseio.com",
-    projectId: "{MY-APP}",
-    storageBucket: "{MY-APP}.appspot.com",
-    messagingSenderId: "547456745674657",
-    appId: "1:8567856785678:web:234232435342554",
-    measurementId: "G-GHERTHERTV"
-  };
-
-window.newFirebase = async function (instancia = false) {
-    randomString = (n, r = '') => {
-        while (n--) r += String.fromCharCode((r = Math.random() * 62 | 0, r += r > 9 ? (r < 36 ? 55 : 61) : 48));
-        return r;
-    };
-    if (instancia == false) {
-        var instancia = 'firebase_' + randomString(50);
-    } else {
-        var instancia = 'firebase_' + instancia;
-    }
-    if (typeof window[instancia] == 'undefined') {
-        window[instancia] = {};
-    }
-
-    window[instancia] = {
-        firebase: {},
-        database: {},
-        initialize: function (e) {
-            window[instancia].firebase = window.firebase;
-            if (window[instancia].firebase.apps.length == 0) {
-                window[instancia].firebase.initializeApp(firebaseConfig);
-                window[instancia].database = window[instancia].firebase.database().ref('/');
+window.newFirebaseKey = {
+    apiKey: "----------",
+    authDomain: "{----------}.firebaseapp.com",
+    databaseURL: "https://{----------}.firebaseio.com",
+    projectId: "{----------}",
+    storageBucket: "{----------}.appspot.com",
+    messagingSenderId: "----------",
+    appId: "1:----------:web:----------",
+    measurementId: "G- ----------"
+}
+window.newFirebaseKey = {
+    apiKey: "AIzaSyCbkmgyaRvtKUYuDMrVDf4QauDhBeWXb1Y",
+    authDomain: "ticksclub-01.firebaseapp.com",
+    databaseURL: "https://ticksclub-01.firebaseio.com",
+    projectId: "ticksclub-01",
+    storageBucket: "ticksclub-01.appspot.com",
+    messagingSenderId: "655793371596",
+    appId: "1:655793371596:web:94f76d0e4d94912e170569",
+    measurementId: "G-2WX4N9DHMD"
+}
+window.newFirebase = {
+    instancia: null,
+    loadedFiles: {},
+    loadScript: function (url) {
+        return new Promise(function (resolve, reject) {
+            if (window.newFirebase.loadedFiles[url[1]] == undefined) {
+                let script = document.createElement('script');
+                script.src = url[0];
+                script.id = url[1] || "";
+                script.async = false;
+                script.onload = function () {
+                    window.newFirebase.loadedFiles[url[1]] = true
+                    resolve(url[0]);
+                };
+                script.onerror = function () { reject(url[0]); };
+                document.body.appendChild(script);
+            } else {
+                resolve(url[0]);
             }
-        },
-        base: function (e) {
-            window[instancia].initialize();
-            window[instancia].database = window[instancia].firebase.database().ref(e);
-            return window[instancia];
-        },
-        set: async function (e) {
-            window[instancia].initialize();
-            return await new Promise((resolve, reject) => {
-                window[instancia].database.set(e).then(function (snapshot) {
-                    resolve(e);
-                })
-            })
-        },
+        });
+    },
 
-        delete: async function () {
-            window[instancia].initialize();
-            return await new Promise((resolve, reject) => {
-                window[instancia].database.set(null).then(function (snapshot) {
-                    resolve(snapshot);
-                })
-            })
-        },
+    install: async function () {
+        let scripts = [
+            ['//cdnjs.cloudflare.com/ajax/libs/firebase/8.0.1/firebase-app.min.js', 'firebase-app'],
+            ['//cdnjs.cloudflare.com/ajax/libs/firebase/8.0.1/firebase-auth.min.js', 'firebase-auth'],
+            ['//cdnjs.cloudflare.com/ajax/libs/firebase/8.0.1/firebase-database.min.js', 'firebase-database']
+        ];
+        let promises = [];
+        scripts.forEach(function (url) { promises.push(window.newFirebase.loadScript(url)); });
+        return Promise.all(promises);
+    },
+    novaInstancia: async function (instancia = false) {
+        $_API = await window.newFirebase.install().then(function () { return true; }).catch(function (a) { return false; })
+        if ($_API == true) {
+            randomString = (n, r = '') => { while (n--) r += String.fromCharCode((r = Math.random() * 62 | 0, r += r > 9 ? (r < 36 ? 55 : 61) : 48)); return r; };
+            if (instancia == false) {
+                instancia = 'firebase_' + randomString(50);
+            } else {
+                instancia = 'firebase_' + instancia;
+            }
+            this.instancia = instancia;
+            if (typeof window[instancia] == 'undefined') {
+                window[instancia] = Object.assign({}, window.firebase);
+            }
+            if (window[instancia].apps && window[instancia].apps.length == 0) {
+                /*
+                |------------------------------------------------------------------
+                |	FUNÇÃO INTERNA DO FRAMEWORK
+                |-------------------------------------------------------------------
+                |	await webserver({type: "GET",async: false,
+                |		function: "firebase::returnFirebaseKeys",
+                |		success:async function (keyFirebase) {window[instancia].initializeApp(keyFirebase.response);}
+                |	});
+                |-------------------------------------------------------------------
+                */
+                // AQUI PARA DEBUG
+                window[instancia].initializeApp(window.newFirebaseKey);
+            }
+            return this;
 
-        push: async function () {
-            window[instancia].initialize();
-            var parametros = arguments
-            return await new Promise((resolve, reject) => {
-                window[instancia].database.once('value').then(function (snapshot) {
-                    var result = snapshot.val()
-                    if (result == null) { result = []; }
-                    if (parametros.length == 1) {
-                        result.push(parametros[0]);
-                    } else {
-                        if (typeof (result[parametros[0]]) != "undefined") {
-                            if (Object.prototype.toString.call(result[parametros[0]]) == '[object Array]') {
-                                result[parametros[0]].push(parametros[1]);
-                            } else if (Object.prototype.toString.call(result[parametros[0]]) == '[object Object]') {
-                                result[parametros[0]] = parametros[1];
-                            } else if (Object.prototype.toString.call(result[parametros[0]]) == '[object String]' || Object.prototype.toString.call(result[parametros[0]]) == '[object Number]') {
-                                var newArray = [result[parametros[0]]];
-                                result[parametros[0]] = newArray;
-                            };
-                        } else {
-                            result[parametros[0]] = parametros[1];
-                        }
-                    }
-                    window[instancia].database.set(result)
-                    resolve(result);
+        } else {
+            console.error("Falha no carregamento das APIs")
+        }
+
+    },
+    base: function (e) {
+        this.database = e;
+        window[this.instancia].database().ref('/' + e)
+        return this;
+    },
+    set: function (e) {
+        return new Promise((resolve, reject) => {
+            window[this.instancia]
+                .database()
+                .ref('/' + this.database)
+                .set(e).then(function (snapshot) { 
+                    resolve(e); 
+                }).catch(function (snapshot) { 
+                    reject(snapshot)
+                })
+        })
+    },
+
+    delete: function () {
+        return new Promise((resolve, reject) => {
+            window[this.instancia]
+                .database().ref('/' + this.database)
+                .set([]).then(function (snapshot) { resolve(snapshot); })
+        })
+    },
+
+    push: async function (parametros) {
+        // var parametros = arguments
+        var myInstancy = this.instancia
+        var myDatabase = this.database
+        return await new Promise((resolve, reject) => {
+            window[myInstancy]
+                .database().ref('/' + myDatabase)
+                .once('value').then((snapshot) => {
+                        var result = snapshot.val()
+                        if (result == null) { result = []; }
+            
+                    
+                        // if (parametros.length == 1) {
+                        
+                        // SE O PARAMETRO É ARRAY
+                        if (Object.prototype.toString.call(parametros) == '[object Array]') {
+
+                            // SE O ORIGINAL É ARRAY
+                            if (Object.prototype.toString.call(result) == '[object Array]'){
+                                result = result.concat(parametros);
+                                console.log('Array -> Array')
+                                // SE O ORIGINAL É OBJECT
+                            } else if (Object.prototype.toString.call(result) == '[object Object]'){
+                                console.log('Array -> Object')
+                                
+                                result[Object.keys(result).length + 1] = parametros;
+                                
+                                // SE O ORIGINAL É QUALQUER OUTRA COISA
+                            }else{
+                                console.log('Array -> qqr coisa')
+                                result = [result, parametros];
+                            }
+                            
+                            
+                        } else if (Object.prototype.toString.call(parametros) == '[object Object]') {
+ 
+                            // SE O ORIGINAL É ARRAY
+                            if (Object.prototype.toString.call(result) == '[object Array]') {
+
+                                result[Object.keys(result).length + 1] = parametros;
+                                // SE O ORIGINAL É OBJECT
+                            } else if (Object.prototype.toString.call(result) == '[object Object]') {
+                                
+                                console.log('Object -> Object')
+                                result = Object.assign(result, parametros);
+                                
+                                // SE O ORIGINAL É QUALQUER OUTRA COISA
+                            } else {
+                                console.log('Object -> qqr coisa')
+                                result = [result, parametros];
+                            }
+                            
+                        } else if (Object.prototype.toString.call(parametros) == '[object String]' || Object.prototype.toString.call(parametros) == '[object Number]') {
+                            console.log('qqr coisa -> qqr coisa')
+                            result = [parametros];
+                        }                    
+                        window[myInstancy].database().ref('/' + myDatabase).set(result).then(function (snapshot) {resolve(snapshot);})
                 });
-            });
-        },
-        get: async function () {
-            window[instancia].initialize();
-            return await new Promise((resolve, reject) => {
-                window[instancia].database.once('value').then(function (snapshot) {
+        });
+    },
+
+    get: async function () {
+        return await new Promise((resolve, reject) => {
+            window[this.instancia]
+                .database().ref('/' + this.database)
+                .once('value').then((snapshot) => {
                     resolve(snapshot.val());
                 })
-            })
-        },
-        onUpdate: function (retorno) {
-            window[instancia].initialize();
-            window[instancia].database.on('value', function (snapshot) {
-                retorno(snapshot.val());
-            });
-        },
-        onChanged: function (retorno) {
-            window[instancia].initialize();
-            window[instancia].database.on('child_changed', function (snapshot) {
-                retorno(snapshot.val());
-            });
-        },
-        onAdded: function (retorno) {
-            window[instancia].initialize();
-            window[instancia].database.on('child_added', function (snapshot) {
-                retorno(snapshot.val());
-            });
-        },
-        onDelete: function (retorno) {
-            window[instancia].initialize();
-            window[instancia].database.on('child_removed', function (snapshot) {
-                retorno(snapshot.val());
-            });
-        }
-    };
+        })
+    },
 
-    console.log("Firebase connected:")
-    console.log("Instance:", instancia)
-    return window[instancia];
+    onUpdate: function (retorno) {
+        window[this.instancia]
+            .database().ref('/' + this.database)
+            .on('value', function (snapshot) {
+                retorno(snapshot.val());
+            });
+    },
+
+    onChanged: function (retorno) {
+        window[this.instancia]
+            .database().ref('/' + this.database)
+            .on('child_changed', function (snapshot) {
+                retorno(snapshot.val());
+            });
+    },
+
+    onAdded: function (retorno) {
+        window[this.instancia]
+            .database().ref('/' + this.database)
+            .on('child_added', function (snapshot) {
+                retorno(snapshot.val());
+            });
+    },
+
+    onDelete: function (retorno) {
+        window[this.instancia]
+            .database().ref('/' + this.database)
+            .on('child_removed', function (snapshot) {
+                retorno(snapshot.val());
+            });
+    }
 }
 
 
+var minhaInstancia = await window.newFirebase.novaInstancia('XABLAU');
 
-var minhaInstancia = await window.newFirebase('XABLAU');
-
-minhaInstancia.base('usuario/profile/data').set({ 'avatar': 'hog87h50gh45hgy.png' }).then(function (snapshot) {
+minhaInstancia.base('usuario').push(['aaa','bbb','ccc','dddd']).then(function (snapshot) {
     console.log("push:", snapshot)
 }, function (snapshot) {
     console.log("push:", snapshot)
